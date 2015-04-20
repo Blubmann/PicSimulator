f04cee3f9dee68400ad687c0170f1505382e1df8
Fabian <irgendwas@gmail.com>
2015-04-20 13:35:27 +0200

    Registertabelle implementieren

--- PicSimulator/src/RegTable.java
+++ PicSimulator/src/RegTable.java
@@ -1,87 +1,102 @@
 import java.util.Vector;
 
+import javax.swing.table.*;
 import javax.swing.DefaultCellEditor;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class RegTable extends JTable {
-	private String[][] bank0 = new String[128][2];
-    private String[] titel = new String[2];
-    private DefaultTableModel dm0;
-	public RegTable(JScrollPane scrollPane){
-		JTable table = new JTable();
-		scrollPane.setViewportView(table);
-		table.setModel(new javax.swing.table.DefaultTableModel(
-	            new Object [][] {
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null},
-	                {null, null}
-	            },
-	            new String [] {
-	                "Adresse", "Value"
-	            }
-	        ) {
-	            Class[] types = new Class [] {
-	                java.lang.String.class, java.lang.Integer.class
-	            };
-	            boolean[] canEdit = new boolean [] {
-	                false, false
-	            };
-
-	            public Class getColumnClass(int columnIndex) {
-	                return types [columnIndex];
-	            }
-
-	            public boolean isCellEditable(int rowIndex, int columnIndex) {
-	                return canEdit [columnIndex];
-	            }
-	        });
-		dm0 = new DefaultTableModel(bank0, titel);
-        for (int i = 0; i < 128; i++) {
-            if (i != 1 && i != 2 && i != 3 && i != 4 && i != 5 && i != 6 && i != 8 && i != 9 && i != 10 && i != 11) {
-                dm0.setValueAt("Register " + i, i, 0);
+	private Object[][] bank0 = new Object[128][2];
+	private Object[][] bank1 = new Object[128][2];
+    private String[] titel = {"Register(in hex)","Wert(in hex)"};
+    private int[] register0 = ParDecInt.reg.bank0;
+    private int[] register1 = ParDecInt.reg.bank1;
+    private JTable table = new JTable();
+    
+    public RegTable(JScrollPane scrollPane){
+    	int count=0;
+    	for(int k=0;k<128;k++){
+    		bank0[k][0]=("Register " +Integer.toHexString(k));
+    		bank0[k][1]=Integer.toHexString(register0[count]);
+    		count++;
+    	}
+    	bank0[1][0]= "TMR0";
+    	bank0[2][0] = "PCL";
+    	bank0[3][0] = "STATUS";
+    	bank0[4][0] = "FSR";
+    	bank0[5][0] = "PORTA";
+    	bank0[6][0] = "PORTB";
+    	bank0[8][0] = "EEDATA";
+    	bank0[9][0] = "EEADR";
+    	bank0[10][0] = "PCLATH";
+    	bank0[11][0] = "INTCON";
+    	
+    	bank1[1][0] = "OPTION";
+    	bank1[2][0] = "PCL";
+    	bank1[3][0] = "STATUS";
+    	bank1[4][0] = "FSR";
+    	bank1[5][0] = "TRISA";
+    	bank1[6][0] = "TRISB";
+    	bank1[8][0] = "ECON1";
+    	bank1[9][0] = "ECON2";
+    	bank1[10][0] = "PCLATH";
+    	bank1[11][0] = "INTCON";
+    	
+    	scrollPane.setViewportView(table);	
+    	table.setModel(new DefaultTableModel(bank0,titel){
+    		boolean[] canEdit = new boolean [] {false, true};
+    		public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
-            dm0.setValueAt(ParDecInt.reg.bank0[i], i, 1);
-        }
-        table.setModel(dm0);
-        table.getColumnModel().getColumn(0).setPreferredWidth(100);
+    	});	
+    	table.getColumnModel().getColumn(0).setPreferredWidth(100);
 		table.getColumnModel().getColumn(0).setMaxWidth(250);
 		table.getColumnModel().getColumn(1).setResizable(false);
-		table.getColumnModel().getColumn(1).setPreferredWidth(100);
-	}
+		table.getColumnModel().getColumn(1).setPreferredWidth(100);	
+    }
+    
+    public void updateTable0(JScrollPane scrollPane){
+    	int count=0;
+    	for(int k=0;k<128;k++){
+    		if (k != 1 && k != 2 && k != 3 && k != 4 && k != 5 && k != 6 && k != 8 && k != 9 && k != 10 && k != 11){
+    			bank0[k][0]=("Register " +Integer.toHexString(k));
+    		}
+			bank0[k][1]=Integer.toHexString(register0[count]);
+    		count++;
+    	}
+    	scrollPane.setViewportView(table);
+    	table.setModel(new DefaultTableModel(bank0,titel){
+    		boolean[] canEdit = new boolean [] {false, true};
+    		public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+    	});	
+    	table.getColumnModel().getColumn(0).setPreferredWidth(100);
+		table.getColumnModel().getColumn(0).setMaxWidth(250);
+		table.getColumnModel().getColumn(1).setResizable(false);
+		table.getColumnModel().getColumn(1).setPreferredWidth(100);	
+    }
+    
+    public void updateTable1(JScrollPane scrollPane){
+    	int count=0;
+    	for(int k=0;k<128;k++){
+    		if (k != 1 && k != 2 && k != 3 && k != 4 && k != 5 && k != 6 && k != 8 && k != 9 && k != 10 && k != 11){
+    			bank1[k][0]=("Register " + Integer.toHexString(k+128));
+    		}
+			bank1[k][1]=Integer.toHexString(register1[count]);
+    		count++;
+    	}
+    	scrollPane.setViewportView(table);	
+    	table.setModel(new DefaultTableModel(bank1,titel){
+    		boolean[] canEdit = new boolean [] {false, true};
+    		public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+    	});	
+    	table.getColumnModel().getColumn(0).setPreferredWidth(100);
+		table.getColumnModel().getColumn(0).setMaxWidth(250);
+		table.getColumnModel().getColumn(1).setResizable(false);
+		table.getColumnModel().getColumn(1).setPreferredWidth(100);	
+    }
 }
